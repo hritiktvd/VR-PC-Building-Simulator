0b67c385c83d56c4969939b28ec48f1aa0b7e79b
mangalm2077 <mangalm@purdue.edu>
2022-03-04 23:19:57 -0500

    added spawn manager

--- Assets/Scripts/LobbyBehaviour.cs
+++ Assets/Scripts/LobbyBehaviour.cs
@@ -108,7 +108,7 @@ public class LobbyBehaviour : MonoBehaviourPunCallbacks
     public override void OnJoinedRoom()
     {
         Debug.Log("The Local player: " + PhotonNetwork.NickName + " joined to " + PhotonNetwork.CurrentRoom.Name + " Player count " + PhotonNetwork.CurrentRoom.PlayerCount);
-        PhotonNetwork.LoadLevel("mod_trainer_game");
+        PhotonNetwork.LoadLevel("Sample Scene");
     }
 
     public override void OnPlayerEnteredRoom(Player newPlayer)
